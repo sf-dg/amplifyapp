089caabbef7b1c2d4a2a57ffae62eedc4471cffc
Sami Fadli <sami.fadli@direkt-gruppe.de>
2021-02-24 10:31:03 +0100

    adds typescript

--- src/App.js
+++ src/App.js
@@ -6,9 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edited        
-        </p>
+        <p>Edited</p>
         <a
           className="App-link"
           href="https://reactjs.org"
